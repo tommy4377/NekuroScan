8cf5b7fe73727fed617c64692aa250fa4ce20e27
tommy4377 <129632197+tommy4377@users.noreply.github.com>
2025-09-17 09:57:38 +0200

    Update App.jsx

--- kuro-reader/frontend/src/App.jsx
+++ kuro-reader/frontend/src/App.jsx
@@ -1,33 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ChakraProvider, Box } from '@chakra-ui/react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { theme } from './styles/theme';
 import Navigation from './components/Navigation';
+import Welcome from './pages/Welcome';
 import Home from './pages/Home';
 import Search from './pages/Search';
 import MangaDetails from './pages/MangaDetails';
 import ReaderPage from './pages/ReaderPage';
 import Library from './components/Library';
-import Login from './pages/Login';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Router>
-        <Box minH="100vh" bg="gray.900">
-          <Navigation />
-          
-          <Box pt="60px" pb={{ base: "60px", md: "0" }}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/search" element={<Search />} />
-              <Route path="/library" element={<Library />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/manga/:source/:id" element={<MangaDetails />} />
-              <Route path="/read/:source/:mangaId/:chapterId" element={<ReaderPage />} />
-            </Routes>
-          </Box>
-        </Box>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/home" element={
+            <Box minH="100vh" bg="gray.900">
+              <Navigation />
+              <Box pt="60px">
+                <Home />
+              </Box>
+            </Box>
+          } />
+          <Route path="/search" element={
+            <Box minH="100vh" bg="gray.900">
+              <Navigation />
+              <Box pt="60px">
+                <Search />
+              </Box>
+            </Box>
+          } />
+          <Route path="/library" element={
+            <Box minH="100vh" bg="gray.900">
+              <Navigation />
+              <Box pt="60px">
+                <Library />
+              </Box>
+            </Box>
+          } />
+          <Route path="/manga/:source/:id" element={
+            <Box minH="100vh" bg="gray.900">
+              <Navigation />
+              <Box pt="60px">
+                <MangaDetails />
+              </Box>
+            </Box>
+          } />
+          <Route path="/read/:source/:mangaId/:chapterId" element={<ReaderPage />} />
+        </Routes>
       </Router>
     </ChakraProvider>
   );
