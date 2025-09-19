908b7b47a82edb328439370005d226482c922367
tommy4377 <129632197+tommy4377@users.noreply.github.com>
2025-09-19 10:08:26 +0200

    Create Settings.jsx

---
+++ kuro-reader/frontend/src/pages/Settings.jsx
@@ -0,0 +1,14 @@
+// frontend/src/pages/Settings.jsx
+import React from 'react';
+import { Container, VStack, Heading } from '@chakra-ui/react';
+
+export default function Settings() {
+  return (
+    <Container maxW="container.xl" py={8}>
+      <VStack spacing={6}>
+        <Heading>Impostazioni</Heading>
+        {/* Implementa le impostazioni generali qui */}
+      </VStack>
+    </Container>
+  );
+}
