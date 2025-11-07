20f1e8683811f0c70fdb9cd542e0d298607e01be
tommy4377 <129632197+tommy4377@users.noreply.github.com>
2025-11-07 17:45:05 +0100

    bug fix

---
+++ kuro-reader/frontend/scripts/patch-package.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+// Stub per patch-package - non fa nulla
+// Questo previene errori quando rollup cerca di eseguire patch-package
+console.log('patch-package: no patches to apply');
+process.exit(0);
+
